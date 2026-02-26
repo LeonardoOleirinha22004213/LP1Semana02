1f1f424861f8cac4503cbd8bd92a1b065c4eb906
Shorinoi <leonardo.oleirinha.work@gmail.com>
2026-02-26 16:30:39 +0000

    Add questions for Temperature and Unity

---
+++ TempConvert/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TempConvert
+{
+    public class Program
+    {
+        float temperatura = Convert.ToInt32(Console.ReadLine());
+        private static void Main(string[] args)
+        {
+            Console.Write("Input Temperature Value: ");
+            float temperatura = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input Unity of Origin (C for Celciu and F for Farrenheit): ");
+            string unidadeOrigem = Console.ReadLine().ToUpper();
+        }
+    }
+}
